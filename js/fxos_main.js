64923eed9b50b432714e9f0b4e41e377ba8f9522
ivamilo <ivamilo@college.harvard.edu>
2014-05-07 03:05:46 -0400

    slight modification

--- js/fxos_main.js
+++ js/fxos_main.js
@@ -4,130 +4,6 @@ function debug(message) {
   console.log('DEBUG:(' + dbgcnt + ') ' + message);
 }
 
-var sender;
-var fileBlob;
-function startSending() {
-  // Get app file
-  var request = window.navigator.mozApps.getSelf();
-  
-  request.onsuccess = function() {
-    if (request.result) {
-      // Pull the origin of the app out of the App object
-      var thisApp = request.result;
-      console.log('Name of current app: ' + thisApp.manifest.name);
-      console.log('App origin: ' + thisApp.origin);
-      
-      var origin = thisApp.origin.split('app://')[1];
-      var manifestOrigin = thisApp.manifestURL.split('app://')[1];
-      
-      // 'apps' storage is the /data/ folder
-      var apps = window.navigator.getDeviceStorage('apps');
-      var manifestReq = apps.get('local/webapps/' + manifestOrigin);
-      
-      manifestReq.onsuccess = function () {
-        if (this.result) {
-          var file = this.result;
-          alert('Retrieved manifest!');
-
-          var name = file.name;
-          console.log('File "' + name + '" successfully retrieved from the app storage area');
-
-          console.log('Sending file.');
-          fileBlob = new Blob([file.slice()], {type:''});
-          fileBlob.name = name;
-
-          startDiscovery();
-        }
-      }
-      manifestReq.onerror = function () {
-        console.warn('Unable to get the file: ' + this.error);
-        debug('Error: unable to get file.');
-      }
-
-      var zipReq = apps.get('local/webapps/' + origin + '/application.zip');
-      zipReq.onsuccess = function () {
-        if (this.result) {
-          var file = this.result;
-          var name = file.name;
-          console.log('File "' + name + '" successfully retrieved from the app storage area');
-        
-        }
-      }
-
-      zipReq.onerror = function () {
-        console.warn('Unable to get the file: ' + this.error);
-        debug('Error: unable to get file.');
-      }
-    }
-  }
-
-  // Couldn't get app data
-  request.onerror = function() {
-    // Display error name from the DOMError object
-    console.warn("Error: " + request.error.name);
-  }
-};
-
-
-function startDiscovery () {
-  if ('mozNfc' in window.navigator) {
-    console.log('NFC enabled - sending file via NFC');
-    sender = navigator.mozNfc;
-    //navigator.mozSetMessageHandler('activity', NfcActivityHandler);
-
-    // var data = nfcUI.getActivityData();
-    // nfcUI.p2p = true;
-    
-    window.navigator.mozNfc.onpeerready = function(event) {
-      debug('In onpeerready handler' + JSON.stringify(event.detail));
-      var nfcdom = window.navigator.mozNfc;
-      var nfcPeer = nfcdom.getNFCPeer(event.detail);
-      
-      var req = nfcPeer.sendFile(fileBlob);
-      req.onsuccess = (function() {
-        console.log('SEND FILE successfully');
-      });
-      req.onerror = (function() {
-        console.log('SEND FILE FAILED');
-      });
-     };
-  }
-  // Device is only bluetooth enabled
-  else if ('mozBluetooth' in window.navigator) {
-    console.log('NFC disabled - sending file via Bluetooth only');
-    var bluetooth = window.navigator.mozBluetooth;
-    var settings = window.navigator.mozSettings;
-    if (!settings || !bluetooth) {
-      console.warn('BT Setup failed.');
-      return;
-    }
-
-    if (!bluetooth.enabled) {
-      console.log('Enabling bluetooth.');
-      settings.createLock().set({'bluetooth.enabled': true});
-    }
-
-    var adapter;
-    var btreq = bluetooth.getDefaultAdapter();
-    btreq.onsuccess = function() {
-      adapter = this.result;
-      var pairedreq = adapter.getPairedDevices();
-      BTSend();
-    }
-
-    btreq.onerror = function() {
-      console.warn(btreq.error.name);
-      console.warn('Getting adapter failed.');
-    }
-    
-  }
-  else {
-    console.warn('Unable to enable NFC or Bluetooth - file send failed.');
-    debug('Error: unable to enable NFC or Bluetooth.');
-  }  
-};
-
-
 var apps = window.navigator.getDeviceStorage('apps');
 
 function getAppFile(filename, callback) {
@@ -202,7 +78,6 @@ window.onload = function onload() {
   //shareUI.setMessageArea('#area');
   $('#startbutton').bind('click', function(event, ui) {
     $('#buttontext').text('Sharing App');
-    //startSending();
     BTSend();
   });
 
