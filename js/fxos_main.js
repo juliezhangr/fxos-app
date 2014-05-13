049eff61c384f24a0651af431221c396d02312a1
ivamilo <ivamilo@college.harvard.edu>
2014-05-12 22:08:08 -0400

    nfc and bluetooth

--- js/fxos_main.js
+++ js/fxos_main.js
@@ -4,10 +4,77 @@ function debug(message) {
   console.log('DEBUG:(' + dbgcnt + ') ' + message);
 }
 
-var sender;
-var fileBlob;
-function startSending() {
-  // Get app file
+var apps = window.navigator.getDeviceStorage('apps');
+
+function getAppFile(filename, callback) {
+    var req = apps.get(filename);
+    req.onsuccess = function() {
+        callback(req.result);
+    };
+    req.onerror = function() {
+        console.error('Failed to get app file', filename);
+    };
+}
+
+function NFCSend() {
+    console.log('NFC enabled - sending file via NFC');
+    sender = navigator.mozNfc;
+    navigator.mozSetMessageHandler('activity', NfcActivityHandler);
+
+    getAppFile('local/webapps/' + thisApp.origin.split('app://')[1] + '/application.zip', function(fileBlob) {
+        window.navigator.mozNfc.onpeerready = function(event) {
+            debug('In onpeerready handler' + JSON.stringify(event.detail));
+            var nfcdom = window.navigator.mozNfc;
+            var nfcPeer = nfcdom.getNFCPeer(event.detail);
+            
+            var req = nfcPeer.sendFile(fileBlob);
+            req.onsuccess = (function() {
+              console.log('SEND FILE successfully');
+            });
+            req.onerror = (function() {
+              console.log('SEND FILE FAILED');
+            });
+        };
+    }
+}
+
+function BTSendApp(thisApp) {
+    var origin = thisApp.origin.split('app://')[1];
+    var manifestOrigin = thisApp.manifestURL.split('app://')[1];
+
+    getAppFile('local/webapps/' + manifestOrigin, function(file) {
+        var blobs = [file];
+        var names = [file.name]
+        console.log('File "' + name + '" successfully retrieved from the app storage area');
+        getAppFile('local/webapps/' + origin + '/application.zip', function(file2) {
+            blobs.push(file2);
+            names.push(file2.name);
+            console.log('File "' + file2.name + '" successfully retrieved from the app storage area');
+
+            var a = new MozActivity({
+                name: 'share',
+                data: {
+                    number: blobs.length,
+                    blobs: blobs,
+                    filenames: names,
+                    filepaths: names
+                }
+            });
+            a.onsuccess = function() {
+                console.log("share activity success")
+            };
+
+            a.onerror = function(e) {
+                console.warn('share activity error:', a.error.name);
+            };
+    });
+
+    });
+
+}
+
+function BTSend() {
+    
   var request = window.navigator.mozApps.getSelf();
   
   request.onsuccess = function() {
@@ -16,59 +83,8 @@ function startSending() {
       var thisApp = request.result;
       console.log('Name of current app: ' + thisApp.manifest.name);
       console.log('App origin: ' + thisApp.origin);
-      
-      var origin = thisApp.origin.split('app://')[1];
-      var manifestOrigin = thisApp.manifestURL.split('app://')[1];
-      
-      // 'apps' storage is the /data/ folder
-      var apps = window.navigator.getDeviceStorage('apps');
-      var manifestReq = apps.get('local/webapps/' + manifestOrigin);
-      
-      manifestReq.onsuccess = function () {
-        if (this.result) {
-          var file = this.result;
-          alert('Retrieved manifest!');
-
-          var name = file.name;
-          console.log('File "' + name + '" successfully retrieved from the app storage area');
-
-          console.log('Sending file.');
-          fileBlob = new Blob([file.slice()], {type:''});
-          fileBlob.name = file.name;
-
-          startDiscovery();
-
-          // fileBlob = new Blob([], {type:''});
-      
-          // var sending = sender.sendFile(fileBlob);
-
-      //   sending.onsuccess = function() {
-      //     navigator.mozSettings.createLock().set({'bluetooth.enabled': false});
-      //   }
-      //   sending.onerror = function() { 
-      //     debug('sendFile FAILED.'); 
-      //   }
-        }
-      }
-      manifestReq.onerror = function () {
-        console.warn('Unable to get the file: ' + this.error);
-        debug('Error: unable to get file.');
-      }
-
-      var zipReq = apps.get('local/webapps/' + origin + '/application.zip');
-      zipReq.onsuccess = function () {
-        if (this.result) {
-          var file = this.result;
-          var name = file.name;
-          console.log('File "' + name + '" successfully retrieved from the app storage area');
-        
-        }
-      }
-
-      zipReq.onerror = function () {
-        console.warn('Unable to get the file: ' + this.error);
-        debug('Error: unable to get file.');
-      }
+      console.log(thisApp);
+      BTSendApp(thisApp);
     }
   }
 
@@ -77,91 +93,21 @@ function startSending() {
     // Display error name from the DOMError object
     console.warn("Error: " + request.error.name);
   }
-};
 
-function NfcActivityHandler(activity) {
-  var activityName = activity.source.name;
-  var data = activity.source.data;
-  console.log('Received NFC activity: ' + activityName);
-  alert('Received activity!');
 }
-
-function startDiscovery () {
-  // Use nfc handover to send file
-  if ('mozNfc' in window.navigator) {
-    console.log('NFC enabled - sending file via NFC');
-    sender = navigator.mozNfc;
-    navigator.mozSetMessageHandler('activity', NfcActivityHandler);
-
-    window.navigator.mozNfc.onpeerready = function(event) {
-      debug('In onpeerready handler' + JSON.stringify(event.detail));
-      var nfcdom = window.navigator.mozNfc;
-      var nfcPeer = nfcdom.getNFCPeer(event.detail);
-      
-      var req = nfcPeer.sendFile(fileBlob);
-      req.onsuccess = (function() {
-        console.log('SEND FILE successfully');
-      });
-      req.onerror = (function() {
-        console.log('SEND FILE FAILED');
-      });
-     };
-  }
-  // Device is only bluetooth enabled
-  else if ('mozBluetooth' in window.navigator) {
-    console.log('NFC disabled - sending file via Bluetooth only');
-    var bluetooth = window.navigator.mozBluetooth;
-    var settings = window.navigator.mozSettings;
-    if (!settings || !bluetooth) {
-      console.warn('BT Setup failed.');
-      return;
-    }
-
-    if (!bluetooth.enabled) {
-      console.log('Enabling bluetooth.');
-      settings.createLock().set({'bluetooth.enabled': true});
-    }
-
-    var adapter;
-    var btreq = bluetooth.getDefaultAdapter();
-    btreq.onsuccess = function() {
-      adapter = this.result;
-      var pairedreq = adapter.getPairedDevices();
-      pairedreq.onsuccess = function () {
-        var btpeers = this.result;
-        console.log(btpeers);
-        // TODO: figure out how to pull up list of paired peers, hardcoded for now yay
-        var sendreq = adapter.sendFile(btpeers[0].address, fileBlob);
-        sendreq.onsuccess = function() {
-          alert('sucessss!');
-        }
-        sendreq.onerror = function() {
-          console.warn(this.error.name);
-        }
-      }
-    }
-
-    btreq.onerror = function() {
-      console.warn(btreq.error.name);
-      console.warn('Getting adapter failed.');
-    }
-    
-  }
-  else {
-    console.warn('Unable to enable NFC or Bluetooth - file send failed.');
-    debug('Error: unable to enable NFC or Bluetooth.');
-  }  
-};
-
 
 window.onload = function onload() {
   $('#startbutton').bind('click', function(event, ui) {
     $('#buttontext').text('Sharing App');
-    startSending();
+    if ('mozNfc' in window.navigator) {
+        NFCSend();
+    }
+    else if ('mozBluetooth' in window.navigator) {
+        BTSend();
+    }
+    else {
+        console.warn('Unable to enable NFC or Bluetooth - file send failed.');
+        debug('Error: unable to enable NFC or Bluetooth.');
+    }  
   });
-
-  // Used for debugging BT/NFC connection
-  // $('#devicelistbutton').bind('click', function(event, ui) {
-  //   startDiscovery();
-  // });
 };
